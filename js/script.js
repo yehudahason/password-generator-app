fe3c19d54a2924b232d4235ec74c57cf479308cb
Yehuda Hason <yehudaxt@gmail.com>
2025-12-29 17:36:11 +0200

    final

--- js/script.js
+++ js/script.js
@@ -112,6 +112,7 @@ function copyToClipboard(text) {
 function setPasswordDisplay(password) {
   const passwordDisplay = document.querySelector(".password");
   passwordDisplay.textContent = password;
+  passwordDisplay.style.color = "white";
 }
 
 function setCopiedDisplay(set) {
